95a7b982f07976c6edc9ce188285f8dd98a95e05
kk <kamaihamaiha@163.com>
2022-08-04 09:10:09 +0800

    update: 指针当做参数传递

---
+++ study_c_from_hello_code/pointer_part/demo-3/demo-3/demo-3.cpp
@@ -0,0 +1,50 @@
+﻿#include <stdio.h>
+
+void swap(int *, int *);
+
+void swap2(void*, void*, int size);
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+
+    printf("a=%d\n", a);
+    printf("b=%d\n", b);
+
+    swap2(&a, &b, sizeof(a));
+
+    printf("a=%d\n", a);
+    printf("b=%d\n", b);
+
+    
+    return 0;
+}
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void swap2(void* x, void* y, int size) {
+    char temp[128];
+    char *xc = (char*)x;
+    char *yc = (char*)y;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        temp[i] = xc[i];
+    }
+    
+    for (size_t i = 0; i < size; i++)
+    {
+        xc[i] = yc[i];
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        yc[i] = temp[i];
+    }
+
+}
--- study_c_from_hello_code/pointer_part/readme.md
+++ study_c_from_hello_code/pointer_part/readme.md
@@ -3,4 +3,5 @@
 - [概念](./intro.md)
 - [指针运算](./section_1.md)
 - [指针与数组](./section_2.md)
+- [指针传递参数](./section_3.md)
 
