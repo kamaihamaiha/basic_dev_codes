5934191db28478df577bf7e24edf818b17edca5a
kk <kamaihamaiha@gmail.com>
2023-01-11 23:22:01 +0800

    update: 添加头文件和源文件目录

--- CMakeDemo/HelloWord/demo/cmd-dir.cpp
+++ CMakeDemo/HelloWord/demo/cmd-dir.cpp
@@ -1,10 +1,12 @@
 #include <cstdlib>
 #include <string>
 #include <iostream>
+#include "test.h"
 
 using namespace std;
 
 int main(int argc, char** argv){
     cout << "This is cmd-dir:" << endl;
+    test_log();
     return EXIT_SUCCESS;
 }
