68774ef5e2712e69ccc785635ffda108bf156a8c
kk <kamaihamaiha@gmail.com>
2024-05-17 22:34:50 +0800

    python: condition match 复杂匹配和匹配列表

--- study_for_python/basic/condition_match.py
+++ study_for_python/basic/condition_match.py
@@ -13,3 +13,33 @@ match score:
         print('score is D')
     case _: # 表示匹配到其他任何情况
         print('score is valid')
+
+# 复杂匹配
+age = 15
+match age:
+    case x if x < 10:
+        print(f'< 10 years old: {x}')        
+    case 10:
+        print("10 years old.")
+    case 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18:
+        print('11~18 years old.')
+    case 19:
+        print('19 years old.')
+    case _:
+        print('not sure.')      
+
+# 匹配列表
+# args = ['gcc', 'hello.c', 'world.c']
+# args = ['gcc']
+args = ['clean']
+
+match args:
+    case ['gcc']:
+        print('gcc: missing source file(s).')
+    case ['gcc', file_1, *files]:
+        print('gcc compile: ' + file_1 + ", ".join(files))         
+    case ['clean']:
+        print('clean...')
+    case _:
+        print('invalide command.') 
+
