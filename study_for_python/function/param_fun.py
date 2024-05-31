02fec6fde5483b031b19efe886cd38d9b280b21c
kk <kamaihamaiha@gmail.com>
2024-05-31 23:20:02 +0800

    python: param_fun

--- study_for_python/function/param_fun.py
+++ study_for_python/function/param_fun.py
@@ -42,3 +42,80 @@ print(calc(1, 2))
 scores = [100, 1, 2]
 print(calc(*scores))
 print(calc(scores[0], scores[1]))
+
+# 关键字参数
+def person(name, age, **kw):
+     print('name:', name, 'age:', age, 'other:', kw)
+
+person('Michael', 30)     
+person('Bob', 31, city='HongKong')     
+person('Messi', 36, city='Shanghai', gender='Male')
+
+info = {'city': 'Beijing', 'job': "Engineer"}
+person('Jack', 24, **info)
+
+# 命名关键字参数；上面的参数名可以随便输入，不受限制；如果想限制的话看下面的
+def person(name, age, *, city, job):
+     print(name, age, city, job)
+
+person('Jack', 25, city='NY', job='Teacher')     
+
+# 如果参数有可变参数，命名关键字参数的前面不用加 * 分割了
+def person(name, age, *args, city, job):
+     print(name, age, args, city, job)
+
+
+# 参数组合： 可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用。但是请注意，参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数
+     
+def f1(a, b, c = 0, *args, **kw):
+     print('a=', a, 'b = ', b, 'c = ', c, 'args = ', args, 'kw = ', kw)
+
+f1(1,2)
+f1(1,2,3)
+f1(1,2,3,'arg1')          
+f1(1,2,3,'arg1','arg2')          
+f1(1,2,3,'arg1','arg2', city='shagnhai')          
+f1(1,2,3,'arg1','arg2', city='shagnhai', gender='m')
+
+def f2(a,b,c=0, *, d, **kw):
+     print('a=',a, 'b=',b,'c=',c, 'd=',d, 'kw=',kw)
+
+args = (1, 2, 3, 4)
+kw = {'age': 22, 'height': 177}
+f1(1,2,3,*args, **kw)
+
+args = (1,2,3)
+kw = {'d': 100, 'x': 99}
+f2(*args, **kw) # 对于任意函数，都可以通过类似func(*args, **kw)的形式调用它，无论它的参数是如何定义的。
+
+
+# 练习: 以下函数允许计算两个数的乘积，请稍加改造，变成可接收一个或多个数并计算乘积：
+def mul(x, y):
+    return x * y
+
+def mul(x, *y):
+    ret = x
+    for item_y in y:
+        ret = ret * item_y
+    return ret
+
+
+# 测试
+print('mul(5) =', mul(5))
+print('mul(5, 6) =', mul(5, 6))
+print('mul(5, 6, 7) =', mul(5, 6, 7))
+print('mul(5, 6, 7, 9) =', mul(5, 6, 7, 9))
+if mul(5) != 5:
+    print('测试失败!')
+elif mul(5, 6) != 30:
+    print('测试失败!')
+elif mul(5, 6, 7) != 210:
+    print('测试失败!')
+elif mul(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        mul()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
