48ae3248ece67997c9d366b24f5f656317984959
kk <kamaihamaiha@gmail.com>
2024-07-30 08:30:58 +0800

    print & scanf

--- study_for_c/c_from_hello_code/11_scanf/11_scanf/11_scanf.cpp
+++ study_for_c/c_from_hello_code/11_scanf/11_scanf/11_scanf.cpp
@@ -5,9 +5,13 @@
 
 
 #include <iostream>
-
-int main()
-{
+/**
+ * 注意:
+ * 如果 scanf 要存入基本变量，则在变量名前加 &
+ * 如果 scanf 要存入字符数组中，则字符数组名不用加 &
+ * @return
+ */
+int main() {
     char c;
     short s;
     int n;
@@ -15,12 +19,26 @@ int main()
     float f;
     double df;
 
-   // scanf("%hhd %hd %d %ld %f %lf", &c, &s, &n, &l, &f, &df);
-   // printf("%d %d %d %d %2.1f %2.2f\n", c, s, n, l, f, df);
-   // scanf("%hhd", &c);
-   // printf("%c\n", c);
+    // 输入基本数据类型
+    printf("Please input nums: char, short, int, long, float, double");
+    scanf("%hhd %hd %d %ld %f %lf", &c, &s, &n, &l, &f, &df); // 注意第一个参数里面的格式都是数字之间加空格，因此输入数字时，也要用空格隔开
+    printf("%d %d %d %ld %2.1f %2.2f\n", c, s, n, l, f, df);
+
+    // 输入字符
+    char c_;
+    scanf("%hhd", &c_); // 必须输入字符对应的 ASCII值; 因为是当作 整型char输入的
+    printf("%d, %c\n", c_, c_);
+
+    // 清除输入缓冲区中的任何残留字符
+    while (getchar() != '\n');
+
+    char cc;
+    printf("please input char:\n");
+    scanf(" %c\n", &cc);
+    printf("%d, %c\n", cc, cc);
 
     // 输入字符串：C语言没有字符串类型，要用字符数组表示
+    printf("please input char...");
     char str[10];
     scanf("%s", &str[0]);
     printf("%s\n", str);
