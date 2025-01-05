7128f133c611e98970540b61c46091a29684bad5
kk <kamaihamaiha@gmail.com>
2025-01-05 15:58:15 +0800

    响应按键，向右移动

--- study_for_python_book/projects/alien_invasion/game_functions.py
+++ study_for_python_book/projects/alien_invasion/game_functions.py
@@ -2,11 +2,21 @@ import sys
 
 import pygame
 
-def check_events():
+def check_events(ship):
 	# 监视键盘和鼠标事件
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RIGHT:
+					# 只修改飞船的moving_right标记
+					ship.moving_right = True
+					
+			elif event.type == pygame.KEYUP:
+				if event.key == pygame.K_RIGHT:
+					# 只修改飞船的moving_right标记
+					ship.moving_right = False
+
 
 
 def update_screen(screen, settings, ship):
