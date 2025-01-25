513df0880721e40e3a6cc49f3ef3ff515142ed6c
kk <kamaihamaiha@gmail.com>
2025-01-25 15:57:53 +0800

    绘制散点图

--- study_for_python_book/c15/readme.md
+++ study_for_python_book/c15/readme.md
@@ -1,4 +1,5 @@
 ## 绘图
 
 - 使用 matplotlib
-- 
+- [折线图demo](./mpl_squares.py)
+- [散点图]()
---
+++ study_for_python_book/c15/scatter_squares.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+
+
+def draw_scatter_char_single_point(plt):
+    plt.scatter(2, 4, s=200)
+    plt.title('scatter chart')
+    plt.xlabel('x value...', fontsize=16)
+    plt.ylabel('y value...', fontsize=18)
+    plt.tick_params(axis='both', labelsize=15)
+    plt.show()
+
+
+def draw_scatter_char_multi_point(plt):
+    x_values=[1, 2, 3, 4, 5]
+    y_values= [1, 4, 9 , 16, 25]
+    plt.scatter(x_values, y_values, s=200)
+    plt.title('scatter chart')
+    plt.xlabel('x value...', fontsize=16)
+    plt.ylabel('y value...', fontsize=18)
+    plt.tick_params(axis='both', labelsize=15)
+    plt.show()
+
+# 自动绘制批量的点
+def draw_scatter_char_multi_point_auto(plt):
+    x_values = list(range(1, 1001))
+    y_values = [x**2 for x in x_values]
+    plt.scatter(x_values, y_values, s=6)
+    plt.title('scatter chart')
+    plt.xlabel('x value...', fontsize=16)
+    plt.ylabel('y value...', fontsize=18)
+    plt.tick_params(axis='both', labelsize=15)
+
+    # 设置坐标轴的取值范围
+    plt.axis([0,1100,0,1100000])
+    plt.show()
+
+
+# draw_scatter_char_single_point(plt) 
+# draw_scatter_char_multi_point(plt)  
+draw_scatter_char_multi_point_auto(plt) 
