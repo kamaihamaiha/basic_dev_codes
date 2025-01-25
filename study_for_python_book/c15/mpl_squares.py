a506785a5f7e4cb7a60e4871370c79bb68b03c62
kk <kamaihamaiha@gmail.com>
2025-01-25 15:57:28 +0800

    matplotlib 绘制折线图

--- study_for_python_book/c15/mpl_squares.py
+++ study_for_python_book/c15/mpl_squares.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+plt.plot(input_values,squares, linewidth=5)
+plt.title("this is a demo squares", fontsize=24)
+plt.xlabel("x value...", fontsize=24)
+plt.ylabel("y value...", fontsize=16)
+# 设置刻度标记大小
+plt.tick_params(axis='both', labelsize=14)
 plt.show()
