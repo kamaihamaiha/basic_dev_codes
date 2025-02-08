a7403ddf53f55c247f0b1adff89b448a51b85755
kk <kamaihamaiha@gmail.com>
2025-02-08 21:51:17 +0800

    突出起点和终点

--- study_for_python_book/c15/rw_demo.py
+++ study_for_python_book/c15/rw_demo.py
@@ -8,6 +8,10 @@ def start_walk():
     # 随机数，用来映射随机颜色
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=8)
+
+    # 单独绘制起点和终点
+    plt.scatter(rw.x_values[0], rw.y_values[0], c='green', edgecolors='none', s=22)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=22)
     plt.show()
 
 # start_walk() 
