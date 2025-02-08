23bb5ac8c870c86cbdcc55beb3f6060cba9815d8
kk <kamaihamaiha@163.com>
2025-02-08 13:28:48 +0800

    支持多次漫步

--- study_for_python_book/c15/rw_demo.py
+++ study_for_python_book/c15/rw_demo.py
@@ -1,7 +1,19 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_point()
-plt.scatter(rw.x_values, rw.y_values, s=8)
-plt.show()
+
+def start_walk():
+    rw = RandomWalk()
+    rw.fill_point()
+    plt.scatter(rw.x_values, rw.y_values, s=8)
+    plt.show()
+
+# start_walk() 
+
+# 不断walk，直到主动退出; 注意，要在终端中运行
+while True:
+    start_walk()
+    keep_running = input("make another wald?(y/n):")
+    if keep_running == 'n':
+        break
+
