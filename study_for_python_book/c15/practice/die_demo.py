da3052aeaf89a584241680b37636838c5cf83da2
kk <kamaihamaiha@gmail.com>
2025-02-10 21:22:21 +0800

    统计骰子结果

--- study_for_python_book/c15/practice/die_demo.py
+++ study_for_python_book/c15/practice/die_demo.py
@@ -4,7 +4,14 @@ from die import Die
 die = Die()
 
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     results.append(die.roll())
 
-print(results)    
+# print(results)    
+# 分析结果
+frequencies = []
+for point in range(1, die.num_sides + 1):
+    frequency = results.count(point)
+    frequencies.append(frequency)
+
+print(frequencies)    
