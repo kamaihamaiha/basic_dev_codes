05fd48557afb7ca4319cd2dc2fecf5980531edf3
kk <kamaihamaiha@gmail.com>
2025-02-10 22:25:43 +0800

    绘制直方图

--- study_for_python_book/c15/practice/die_demo.py
+++ study_for_python_book/c15/practice/die_demo.py
@@ -1,10 +1,11 @@
+import pygal
 from die import Die
 
 
 die = Die()
 
 results = []
-for roll_num in range(1000):
+for roll_num in range(1000000):
     results.append(die.roll())
 
 # print(results)    
@@ -15,3 +16,12 @@ for point in range(1, die.num_sides + 1):
     frequencies.append(frequency)
 
 print(frequencies)    
+
+# 结果可视化
+hist = pygal.Bar()
+hist.title = "1000次掷骰子结果"
+hist.x_labels = [1, 2, 3, 4, 5, 6]
+hist.x_title = 'points'
+hist.y_title = '次数'
+hist.add('六面骰子', frequencies)
+hist.render_to_file('die_test.svg')
