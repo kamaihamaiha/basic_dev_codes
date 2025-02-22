7a1e41803c4fc8633b8319c1aa431ce25c24fcd9
kk <kamaihamaiha@gmail.com>
2025-02-22 22:49:08 +0800

    最高气温折线图

---
+++ study_for_python_book/c16_download-data/highs_lows.py
@@ -0,0 +1,26 @@
+import csv
+from matplotlib import pyplot as plt
+
+filename = 'sitka_weather_07-2014.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    # print(header_row)
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+
+    # 遍历每一行的index=1的数据(最高温度)
+    highs = []
+    for row in reader:
+        highs.append(int(row[1]))
+    print(highs) 
+
+    # 绘制图形
+    fig = plt.figure(dpi=128, figsize=(10, 6))
+    plt.plot(highs, c='red')      
+    # 设置图形的格式
+    plt.title("Daily high temperatures, July 2014", fontsize=24)
+    plt.xlabel('', fontsize=16)
+    plt.ylabel("Temperature (F)", fontsize=16)
+    plt.tick_params(axis='both', which='major', labelsize=16)
+    plt.show()     
