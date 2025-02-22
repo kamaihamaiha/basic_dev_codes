ea721d1d8dd9d11c8a775065492ea0f0569eae33
kk <kamaihamaiha@gmail.com>
2025-02-22 23:11:23 +0800

    x 轴添加日期

--- study_for_python_book/c16_download-data/highs_lows.py
+++ study_for_python_book/c16_download-data/highs_lows.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 from matplotlib import pyplot as plt
 
 filename = 'sitka_weather_07-2014.csv'
@@ -10,17 +11,22 @@ with open(filename) as f:
         print(index, column_header)
 
     # 遍历每一行的index=1的数据(最高温度)
+    dates = []
     highs = []
     for row in reader:
+        cur_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(cur_date)
         highs.append(int(row[1]))
     print(highs) 
 
     # 绘制图形
     fig = plt.figure(dpi=128, figsize=(10, 6))
-    plt.plot(highs, c='red')      
+    plt.plot(dates, highs, c='red')      
     # 设置图形的格式
     plt.title("Daily high temperatures, July 2014", fontsize=24)
     plt.xlabel('', fontsize=16)
+    # 绘制斜的日期标签
+    fig.autofmt_xdate()
     plt.ylabel("Temperature (F)", fontsize=16)
     plt.tick_params(axis='both', which='major', labelsize=16)
     plt.show()     
