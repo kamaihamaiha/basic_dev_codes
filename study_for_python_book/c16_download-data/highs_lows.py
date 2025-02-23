4cb4a01c5a355f7a6e6154db12d0cc5c527e6924
kk <kamaihamaiha@gmail.com>
2025-02-23 09:09:04 +0800

    add 年最低温度

--- study_for_python_book/c16_download-data/highs_lows.py
+++ study_for_python_book/c16_download-data/highs_lows.py
@@ -2,7 +2,7 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -13,15 +13,18 @@ with open(filename) as f:
     # 遍历每一行的index=1的数据(最高温度)
     dates = []
     highs = []
+    lows = []
     for row in reader:
         cur_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(cur_date)
         highs.append(int(row[1]))
+        lows.append(int(row[2]))
     print(highs) 
 
     # 绘制图形
     fig = plt.figure(dpi=128, figsize=(10, 6))
     plt.plot(dates, highs, c='red')      
+    plt.plot(dates, lows, c='blue')      
     # 设置图形的格式
     plt.title("Daily high temperatures, July 2014", fontsize=24)
     plt.xlabel('', fontsize=16)
