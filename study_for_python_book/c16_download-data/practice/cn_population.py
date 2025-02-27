647708d41c7ef23e8d40292fba3df931c1bfaa93
kk <kamaihamaiha@163.com>
2025-02-27 13:50:52 +0800

    draw cn last 74 year population

--- study_for_python_book/c16_download-data/practice/cn_population.py
+++ study_for_python_book/c16_download-data/practice/cn_population.py
@@ -5,36 +5,47 @@ from matplotlib import pyplot as plt
 
 # change work dir
 os.chdir('practice')
-file_name = "cn_population.csv"
+file_name = "cn_pop_last74_format.csv"
 with open(file_name) as f:
     reader = csv.reader(f)
 
     years = []
-    populations = []
+    pop_toal = []
+    pop_citizen = []
+    pop_country = []
 
     row_index = 0
+    skip_title = True
+
+    next(reader) # 跳过第一个title
     for row in reader:
-        if row_index == 0:
-            for year_str in row:
-                years.append(year_str[:-1])
-        if row_index == 1:
-            for popu in row:
-                populations.append(int(popu))
-        row_index+=1        
+        year = datetime.strptime(row[0][:-1], '%Y').year
+        citizen = int(row[1])
+        country = int(row[2])
+        total = int(row[3])
+        years.append(year)
+        pop_toal.append(total)
+        pop_citizen.append(citizen)
+        pop_country.append(country)
 
 # 按照时间正序
 years.reverse()
-populations.reverse()
+pop_toal.reverse()
+pop_citizen.reverse()
+pop_country.reverse()
+
 # draw 
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(years, populations, c='red')
-plt.title("china last 20 year population")
+plt.plot(years, pop_toal, c='black')
+plt.plot(years, pop_citizen, c='red')
+plt.plot(years, pop_country, c='green')
+plt.title("china last 74 year population")
 plt.xlabel('', fontsize=16)
+plt.xlim(min(years), max(years))
 fig.autofmt_xdate()
 plt.ylabel('population', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
-plt.xticks(years[::2])
+plt.xticks(years[::5]) 
 plt.show()
-
 
                  
