a2f68c0b90fa996d4ab3fb0314391d240cdbc4be
kk <kamaihamaiha@gmail.com>
2025-03-26 22:52:43 +0800

    可视化 github most stars python repos

--- study_for_python_book/c17_use-api/python_repos.py
+++ study_for_python_book/c17_use-api/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 python_repos_url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(python_repos_url)
@@ -23,5 +25,32 @@ print('Repositories returned: ', len(items))
 # 研究第一个仓库
 first_repo = items[0]
 print('\nKeys:', len(first_repo))
-for key in sorted(first_repo.keys()):
-    print(key)
+# for key in sorted(first_repo.keys()):
+    # print(key)
+
+def show_repo_info(repo):
+    print('repo name: ', repo['name'])    
+    print('repo owner: ', repo['owner']['login'])    
+    print('repo url: ', repo['url'])    
+    print('repo language: ', repo['language'])    
+    print('repo homepage: ', repo['homepage'])    
+    print('repo git url: ', repo['git_url'])    
+    print('repo created_at: ', repo['created_at'])    
+    print('repo stargazers_count: ', repo['stargazers_count'])    
+    print('repo updated_at: ', repo['updated_at'])    
+    print('repo description: ', repo['description'])
+
+names, stars = [], []
+for repo in items:
+    # show_repo_info(repo)
+    names.append(repo['name'])
+    stars.append(repo['stargazers_count'])
+    # print('\n')   
+
+# 可视化
+my_style= LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on Github'
+chart.x_labels = names
+chart.add('', stars)    
+chart.render_to_file('python_repos.svg')
--- study_for_python_book/c17_use-api/readme.md
+++ study_for_python_book/c17_use-api/readme.md
@@ -4,3 +4,6 @@
 
 - GitHub的API让你能够通过API调用来请求各种信息
   - https://api.github.com/search/repositories?q=language:python&sort=stars
+
+- 监视API的速率限制
+  - 大多数API都存在速率限制，即你在特定时间内可执行的请求数存在限制。要获悉你是否接近了GitHub的限制，在浏览器中输入https://api.github.com/rate_limit 可以看到限制信息
