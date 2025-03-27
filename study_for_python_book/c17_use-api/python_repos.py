0f9c64806919e11dbaed08c4164f6ae932591306
kk <kamaihamaiha@gmail.com>
2025-03-27 22:56:34 +0800

    改进图表样式

--- study_for_python_book/c17_use-api/python_repos.py
+++ study_for_python_book/c17_use-api/python_repos.py
@@ -48,8 +48,21 @@ for repo in items:
     # print('\n')   
 
 # 可视化
+def get_config():
+    my_config = pygal.Config()
+    my_config.x_label_rotation = 45
+    my_config.show_legend = False
+    my_config.title_font_size = 24
+    my_config.label_font_size = 14
+    my_config.major_label_font_size = 18
+    my_config.truncate_label = 15
+    my_config.show_y_guides = False
+    my_config.width = 1000
+    return my_config
+
 my_style= LS('#333366', base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+
+chart = pygal.Bar(get_config(), style=my_style, x_label_rotation=45, show_legend=False)
 chart.title = 'Most-Starred Python Projects on Github'
 chart.x_labels = names
 chart.add('', stars)    
