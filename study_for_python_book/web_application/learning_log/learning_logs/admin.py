a01cfbda408ae67901889e892a47f1b429f9cc58
kk <kamaihamaiha@gmail.com>
2025-04-22 23:26:16 +0800

    定义模型 Entry，然后迁移并注册到管理网站

--- study_for_python_book/web_application/learning_log/learning_logs/admin.py
+++ study_for_python_book/web_application/learning_log/learning_logs/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from learning_logs.models import Topic
+from learning_logs.models import Topic, Entry
 
 # 让Django通过管理网站管理我们的模型
 admin.site.register(Topic)
+admin.site.register(Entry)
