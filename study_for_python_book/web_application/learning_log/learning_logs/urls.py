c0b4be41bd254b025482c905ed05a54ce4791255
kk <kamaihamaiha@gmail.com>
2025-04-23 23:55:34 +0800

    创建网页

---
+++ study_for_python_book/web_application/learning_log/learning_logs/urls.py
@@ -0,0 +1,14 @@
+"""定义learning_logs的URL模式"""
+
+# 导入函数 url
+from django.urls import path
+
+# 导入模块 views
+from . import views
+
+urlpatterns = [
+    # 主页
+    path(r'', views.index, name='index'),
+]
+
+
